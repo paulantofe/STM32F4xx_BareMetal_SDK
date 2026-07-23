6190f5dbbd434ca167c77ba62fbcc9028a48b211
Paul Antofe <paul.antofe@gmail.com>
2026-07-23 20:50:39 +0300

    Cleared useless comments in MCU specific header file

--- STM32F4xx_Drivers/Drivers/Inc/stm32f407xx.h
+++ STM32F4xx_Drivers/Drivers/Inc/stm32f407xx.h
@@ -1,13 +1,6 @@
-/*
- * stm32f407xx.h
- *
- *  Created on: Jul 23, 2026
- *      Author: paula
- */
-
 #ifndef INC_STM32F407XX_H_
 #define INC_STM32F407XX_H_
 
 
 
-#endif /* INC_STM32F407XX_H_ */
+#endif
