a6637900212116af37c4e0c7cb057688fb9aaf0d
Paul Antofe <paul.antofe@gmail.com>
2026-07-25 17:37:51 +0300

    Added ROM address

--- STM32F4xx_Drivers/Drivers/Inc/stm32f407xx.h
+++ STM32F4xx_Drivers/Drivers/Inc/stm32f407xx.h
@@ -4,6 +4,7 @@
 #define FLASH_BASEADDR         0x08000000UL
 #define SRAM1_BASEADDR         0x20000000UL
 #define SRAM2_BASEADDR         0x2001C000UL
+#define ROM                    0x1FFF0000UL
 #define SRAM                   SRAM1_BASEADDR
 
 #endif
