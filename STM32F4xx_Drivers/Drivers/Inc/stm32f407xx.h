e41de6d79309766249e8bbb81ec1c9408cc92fd9
Paul Antofe <paul.antofe@gmail.com>
2026-07-25 16:08:07 +0300

    Added base memory addresses

--- STM32F4xx_Drivers/Drivers/Inc/stm32f407xx.h
+++ STM32F4xx_Drivers/Drivers/Inc/stm32f407xx.h
@@ -1,6 +1,9 @@
 #ifndef INC_STM32F407XX_H_
 #define INC_STM32F407XX_H_
 
-
+#define FLASH_BASEADDR         0x08000000UL
+#define SRAM1_BASEADDR         0x20000000UL
+#define SRAM2_BASEADDR         0x2001C000UL
+#define SRAM                   SRAM1_BASEADDR
 
 #endif
