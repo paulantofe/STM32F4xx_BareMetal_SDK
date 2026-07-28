a4b64950d3f1ecea3ba7735428a77e58acb0755c
Paul Antofe <paul.antofe@gmail.com>
2026-07-28 19:38:56 +0300

    Added pin config structures

--- STM32F4xx_Drivers/Drivers/Inc/stm32f407xx_gpio_driver.h
+++ STM32F4xx_Drivers/Drivers/Inc/stm32f407xx_gpio_driver.h
@@ -1,6 +1,26 @@
 #ifndef INC_STM32F407XX_GPIO_DRIVER_H_
 #define INC_STM32F407XX_GPIO_DRIVER_H_
 
+#include "stm32f407xx.h"
 
+/* ------------------- PIN CONFIGURATION STRUCTURES ------------------- */
+
+// Configuration structure for a GPIO pin
+typedef struct {
+	uint8_t GPIO_PinNumber;
+	uint8_t GPIO_PinMode;
+	uint8_t GPIO_PinOType;
+	uint8_t GPIO_PinSpeed;
+	uint8_t GPIO_PinPuPdControl;
+	uint8_t GPIO_PinAFMode;
+} GPIO_PinConfig_t;
+
+// Handle structure for a GPIO pin
+typedef struct {
+	GPIO_RegDef_t *pGPIOx;
+	GPIO_PinConfig_t GPIO_PinConfig;
+} GPIO_Handle_t;
+
+//-------------------------------------------
 
 #endif
