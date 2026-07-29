ecb9811a2b646855c1a083a9c396efdf410280c6
Paul Antofe <paul.antofe@gmail.com>
2026-07-29 10:10:23 +0300

    Added generic macros

--- STM32F4xx_Drivers/Drivers/Inc/stm32f407xx.h
+++ STM32F4xx_Drivers/Drivers/Inc/stm32f407xx.h
@@ -10,6 +10,8 @@
 #define DISABLE                0
 #define SET                    ENABLE
 #define RESET                  DISABLE
+#define GPIO_PIN_SET           SET
+#define GPIO_PIN_RESET         RESET
 
 //-------------------------------------------
 
