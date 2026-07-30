6670b444c863a58dc2ab5f042139c4ecce77ee6e
Paul Antofe <paul.antofe@gmail.com>
2026-07-30 13:09:28 +0300

    Implemented GPIO_PCLKControl API

--- STM32F4xx_Drivers/Drivers/Src/stm32f407xx_gpio_driver.c
+++ STM32F4xx_Drivers/Drivers/Src/stm32f407xx_gpio_driver.c
@@ -9,7 +9,54 @@
  * @retval None
  */
 void GPIO_PCLKControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi) {
-
+	if (EnorDi == ENABLE) {
+		if (pGPIOx == GPIOA)
+			GPIOA_PCLK_EN();
+		else if (pGPIOx == GPIOB)
+			GPIOB_PCLK_EN();
+		else if (pGPIOx == GPIOC)
+			GPIOC_PCLK_EN();
+		else if (pGPIOx == GPIOD)
+			GPIOD_PCLK_EN();
+		else if (pGPIOx == GPIOE)
+			GPIOE_PCLK_EN();
+		else if (pGPIOx == GPIOF)
+			GPIOF_PCLK_EN();
+		else if (pGPIOx == GPIOG)
+			GPIOG_PCLK_EN();
+		else if (pGPIOx == GPIOH)
+			GPIOH_PCLK_EN();
+		else if (pGPIOx == GPIOI)
+			GPIOI_PCLK_EN();
+		else if (pGPIOx == GPIOJ)
+			GPIOJ_PCLK_EN();
+		else if (pGPIOx == GPIOK)
+			GPIOK_PCLK_EN();
+	}
+	else {
+		if (pGPIOx == GPIOA)
+			GPIOA_PCLK_DI();
+		else if (pGPIOx == GPIOB)
+			GPIOB_PCLK_DI();
+		else if (pGPIOx == GPIOC)
+			GPIOC_PCLK_DI();
+		else if (pGPIOx == GPIOD)
+			GPIOD_PCLK_DI();
+		else if (pGPIOx == GPIOE)
+			GPIOE_PCLK_DI();
+		else if (pGPIOx == GPIOF)
+			GPIOF_PCLK_DI();
+		else if (pGPIOx == GPIOG)
+			GPIOG_PCLK_DI();
+		else if (pGPIOx == GPIOH)
+			GPIOH_PCLK_DI();
+		else if (pGPIOx == GPIOI)
+			GPIOI_PCLK_DI();
+		else if (pGPIOx == GPIOJ)
+			GPIOJ_PCLK_DI();
+		else if (pGPIOx == GPIOK)
+			GPIOK_PCLK_DI();
+	}
 }
 
 /**
@@ -18,7 +65,6 @@ void GPIO_PCLKControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi) {
  * @retval None
  */
 void GPIO_Init(GPIO_Handle_t *pGPIOHandle) {
-
 }
 
 /**
