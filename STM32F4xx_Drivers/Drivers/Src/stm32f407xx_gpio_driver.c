2edb76bd990bf0cc9d7fef71262a7f6be20b5191
Paul Antofe <paul.antofe@gmail.com>
2026-07-30 17:09:22 +0300

    Added GPIO_WriteToOutputPin implementation

--- STM32F4xx_Drivers/Drivers/Src/stm32f407xx_gpio_driver.c
+++ STM32F4xx_Drivers/Drivers/Src/stm32f407xx_gpio_driver.c
@@ -164,7 +164,10 @@ uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *pGPIOx) {
  * @retval None
  */
 void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber, uint8_t Value) {
-
+	if (Value == GPIO_PIN_SET)
+		pGPIOx->BSRR |= (1 << PinNumber);
+	else
+		pGPIOx->BSRR &= ~(1 << (PinNumber + 16));
 }
 
 /**
