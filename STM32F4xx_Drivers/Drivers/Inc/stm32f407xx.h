0f001bcffeacf8826d17dca91f7bc915aa0d073a
Paul Antofe <paul.antofe@gmail.com>
2026-08-04 17:27:33 +0300

    Added IRQ Number macros for EXTI lines

--- STM32F4xx_Drivers/Drivers/Inc/stm32f407xx.h
+++ STM32F4xx_Drivers/Drivers/Inc/stm32f407xx.h
@@ -267,4 +267,17 @@ typedef struct {
 
 //-------------------------------------------
 
+
+/* ------------------- Interrupt Request Numbers ------------------- */
+
+#define EXTI0_IRQn             6
+#define EXTI1_IRQn             7
+#define EXTI2_IRQn             8
+#define EXTI3_IRQn             9
+#define EXTI4_IRQn             10
+#define EXTI9_5_IRQn           23
+#define EXTI15_10_IRQn         40
+
+//-------------------------------------------
+
 #endif
