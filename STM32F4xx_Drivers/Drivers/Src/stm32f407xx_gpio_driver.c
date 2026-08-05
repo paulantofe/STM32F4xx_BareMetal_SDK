cbb62ed5b1653526946995eccf73264bff2d0744
Paul Antofe <paul.antofe@gmail.com>
2026-08-05 14:36:08 +0300

    Changed GPIO_PCLKControl implementatoin to a more compact one

--- STM32F4xx_Drivers/Drivers/Src/stm32f407xx_gpio_driver.c
+++ STM32F4xx_Drivers/Drivers/Src/stm32f407xx_gpio_driver.c
@@ -9,53 +9,12 @@
  * @retval None
  */
 void GPIO_PCLKControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi) {
+	uint8_t portCode = GPIO_BASEADDR_TO_CODE(pGPIOx);
 	if (EnorDi == ENABLE) {
-		if (pGPIOx == GPIOA)
-			GPIOA_PCLK_EN();
-		else if (pGPIOx == GPIOB)
-			GPIOB_PCLK_EN();
-		else if (pGPIOx == GPIOC)
-			GPIOC_PCLK_EN();
-		else if (pGPIOx == GPIOD)
-			GPIOD_PCLK_EN();
-		else if (pGPIOx == GPIOE)
-			GPIOE_PCLK_EN();
-		else if (pGPIOx == GPIOF)
-			GPIOF_PCLK_EN();
-		else if (pGPIOx == GPIOG)
-			GPIOG_PCLK_EN();
-		else if (pGPIOx == GPIOH)
-			GPIOH_PCLK_EN();
-		else if (pGPIOx == GPIOI)
-			GPIOI_PCLK_EN();
-		else if (pGPIOx == GPIOJ)
-			GPIOJ_PCLK_EN();
-		else if (pGPIOx == GPIOK)
-			GPIOK_PCLK_EN();
+		RCC->AHB1ENR |= (1 << portCode);
 	}
 	else {
-		if (pGPIOx == GPIOA)
-			GPIOA_PCLK_DI();
-		else if (pGPIOx == GPIOB)
-			GPIOB_PCLK_DI();
-		else if (pGPIOx == GPIOC)
-			GPIOC_PCLK_DI();
-		else if (pGPIOx == GPIOD)
-			GPIOD_PCLK_DI();
-		else if (pGPIOx == GPIOE)
-			GPIOE_PCLK_DI();
-		else if (pGPIOx == GPIOF)
-			GPIOF_PCLK_DI();
-		else if (pGPIOx == GPIOG)
-			GPIOG_PCLK_DI();
-		else if (pGPIOx == GPIOH)
-			GPIOH_PCLK_DI();
-		else if (pGPIOx == GPIOI)
-			GPIOI_PCLK_DI();
-		else if (pGPIOx == GPIOJ)
-			GPIOJ_PCLK_DI();
-		else if (pGPIOx == GPIOK)
-			GPIOK_PCLK_DI();
+		RCC->AHB1ENR &= ~(1 << portCode);
 	}
 }
 
