b3894b90cd11a03fafbfd8ae5effdfe42d5e9e33
Paul Antofe <paul.antofe@gmail.com>
2026-08-10 19:01:19 +0300

    Added SPI_DeInit API implementation

--- STM32F4xx_Drivers/Drivers/Src/stm32f407xx_spi_driver.c
+++ STM32F4xx_Drivers/Drivers/Src/stm32f407xx_spi_driver.c
@@ -45,7 +45,26 @@ void SPI_PCLKControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi) {
 }
 
 void SPI_Init(SPI_Handle_t *pSPIHandle);
-void SPI_DeInit(SPI_RegDef_t *pSPIx);
+void SPI_DeInit(SPI_RegDef_t *pSPIx) {
+	if (pSPIx == SPI1) {
+		SPI1_REG_RESET();
+	}
+	else if (pSPIx == SPI2) {
+		SPI2_REG_RESET();
+	}
+	else if (pSPIx == SPI3) {
+		SPI3_REG_RESET();
+	}
+	else if (pSPIx == SPI4) {
+		SPI4_REG_RESET();
+	}
+	else if (pSPIx == SPI5) {
+			SPI5_REG_RESET();
+	}
+	else if (pSPIx == SPI6) {
+		SPI6_REG_RESET();
+	}
+}
 
 void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t* pTxBuffer, uint32_t len);
 void SPI_ReceiveData(SPI_RegDef_t *pSPIx, uint8_t* pRxBuffer, uint32_t len);
