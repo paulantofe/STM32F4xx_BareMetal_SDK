2ddb0eff5c60c66bed6d775d63d2be5cfa46cf7b
Paul Antofe <paul.antofe@gmail.com>
2026-08-14 20:10:06 +0300

    Standardized gpio driver header file documentation and format

--- STM32F4xx_Drivers/Drivers/Inc/stm32f407xx_gpio_driver.h
+++ STM32F4xx_Drivers/Drivers/Inc/stm32f407xx_gpio_driver.h
@@ -12,10 +12,11 @@
 
 #include "stm32f407xx.h"
 
-/* ------------------- GENERIC MACROS ------------------- */
+/* -------------------------- GENERIC MACROS -------------------------- */
 
 /**
- * @GPIO_MODES
+ * @defgroup GPIO_MODES        Pin Modes
+ * @{
  */
 #define GPIO_MODE_INPUT        0
 #define GPIO_MODE_OUTPUT       1
@@ -24,30 +25,38 @@
 #define GPIO_MODE_IT_FT        4
 #define GPIO_MODE_IT_RT        5
 #define GPIO_MODE_IT_RFT       6
+/** @} */
 
 /**
- * @GPIO_OP_TYPES
+ * @defgroup GPIO_OP_TYPES     Output Types
+ * @{
  */
 #define GPIO_OP_TYPE_PP        0
 #define GPIO_OP_TYPE_OD        1
+/** @} */
 
 /**
- * @GPIO_SPEED
+ * @defgroup GPIO_SPEED        Speeds
+ * @{
  */
 #define GPIO_SPEED_LOW         0
 #define GPIO_SPEED_MEDIUM      1
 #define GPIO_SPEED_FAST        2
 #define GPIO_SPEED_HIGH        3
+/** @} */
 
 /**
- * @GPIO_PUPD
+ * @defgroup GPIO_PUPD         Pull-up/Pull-down
+ * @{
  */
 #define GPIO_NO_PUPD           0
 #define GPIO_PU                1
 #define GPIO_PD                2
+/** @} */
 
 /**
- * @GPIO_PIN_NUMBER
+ * @defgroup GPIO_PIN_NUMBER   Pin Number
+ * @{
  */
 #define GPIO_PIN_NO_0	 	   0
 #define GPIO_PIN_NO_1	 	   1
@@ -65,6 +74,29 @@
 #define GPIO_PIN_NO_13	 	   13
 #define GPIO_PIN_NO_14	 	   14
 #define GPIO_PIN_NO_15	 	   15
+/** @} */
+
+/**
+ * @defgroup GPIO_AF           Alternate functions
+ * @{
+ */
+#define GPIO_AF0               0
+#define GPIO_AF1               1
+#define GPIO_AF2               2
+#define GPIO_AF3               3
+#define GPIO_AF4               4
+#define GPIO_AF5               5
+#define GPIO_AF6               6
+#define GPIO_AF7               7
+#define GPIO_AF8               8
+#define GPIO_AF9               9
+#define GPIO_AF10              10
+#define GPIO_AF11              11
+#define GPIO_AF12              12
+#define GPIO_AF13              13
+#define GPIO_AF14              14
+#define GPIO_AF15              15
+/** @} */
 
 #define GPIO_BASEADDR_TO_CODE(x)	   ((x == GPIOA) ? 0 : \
                                         (x == GPIOB) ? 1 : \
@@ -78,31 +110,31 @@
                                         (x == GPIOJ) ? 9 : \
                                         (x == GPIOK) ? 10 : -1)
 
-//-------------------------------------------
+/* -------------------------------------------------------------------- */
 
 
-/* ------------------- CONFIGURATION STRUCTURES ------------------- */
+/* -------------------------- CONFIGURATION STRUCTURES -------------------------- */
 
-// Configuration structure for a GPIO pin
+/** @brief Configuration structure for GPIO Pin */
 typedef struct {
-	uint8_t GPIO_PinNumber;         /**< possible values from @GPIO_PIN_NUMBER */
-	uint8_t GPIO_PinMode;			/**< possible values from @GPIO_MODES */
-	uint8_t GPIO_PinOType;          /**< possible values from @GPIO_OP_TYPES */
-	uint8_t GPIO_PinSpeed;          /**< possible values from @GPIO_SPEED */
-	uint8_t GPIO_PinPuPdControl;    /**< possible values from @GPIO_PUPD */
-	uint8_t GPIO_PinAFMode;
+	uint8_t GPIO_PinNumber;         /**< possible values from @ref GPIO_PIN_NUMBER */
+	uint8_t GPIO_PinMode;           /**< possible values from @ref GPIO_MODES      */
+	uint8_t GPIO_PinOType;          /**< possible values from @ref GPIO_OP_TYPES   */
+	uint8_t GPIO_PinSpeed;          /**< possible values from @ref GPIO_SPEED      */
+	uint8_t GPIO_PinPuPdControl;    /**< possible values from @ref GPIO_PUPD       */
+	uint8_t GPIO_PinAFMode;         /**< possible values from @ref GPIO_AF         */
 } GPIO_PinConfig_t;
 
-// Handle structure for a GPIO pin
+/** @brief Handle structure for GPIO Pin */
 typedef struct {
 	GPIO_RegDef_t *pGPIOx;
 	GPIO_PinConfig_t GPIO_PinConfig;
 } GPIO_Handle_t;
 
-//-------------------------------------------
+/* ------------------------------------------------------------------------------ */
 
 
-/* ------------------- SUPPORTED APIs ------------------- */
+/* -------------------------- SUPPORTED APIs -------------------------- */
 
 void GPIO_PCLKControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi);
 
@@ -119,6 +151,6 @@ void GPIO_IRQInterruptConfig(IRQn_Type IRQNumber, uint8_t EnorDi);
 void GPIO_IRQPriorityConfig(IRQn_Type IRQNumber, uint8_t IRQPriority);
 void GPIO_IRQHandling(uint8_t PinNumber);
 
-//-------------------------------------------
+/* -------------------------------------------------------------------- */
 
 #endif
