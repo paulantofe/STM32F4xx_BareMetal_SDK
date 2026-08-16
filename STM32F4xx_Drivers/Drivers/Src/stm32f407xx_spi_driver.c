b3994b3e69f6ec3a8102f15b7e20ff8c4a943b73
Paul Antofe <paul.antofe@gmail.com>
2026-08-16 13:57:33 +0300

    Updated SPI_DeInit as SPI4 SPI 5 SPI 6 are N/A

--- STM32F4xx_Drivers/Drivers/Src/stm32f407xx_spi_driver.c
+++ STM32F4xx_Drivers/Drivers/Src/stm32f407xx_spi_driver.c
@@ -94,15 +94,6 @@ void SPI_DeInit(SPI_RegDef_t *pSPIx) {
 	else if (pSPIx == SPI3) {
 		SPI3_REG_RESET();
 	}
-	else if (pSPIx == SPI4) {
-		SPI4_REG_RESET();
-	}
-	else if (pSPIx == SPI5) {
-		SPI5_REG_RESET();
-	}
-	else if (pSPIx == SPI6) {
-		SPI6_REG_RESET();
-	}
 }
 
 void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t* pTxBuffer, uint32_t len);
