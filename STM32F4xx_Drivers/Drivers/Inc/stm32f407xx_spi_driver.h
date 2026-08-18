20b8930d1e7cad3b4611b149e6973c652259db54
Paul Antofe <paul.antofe@gmail.com>
2026-08-18 18:07:07 +0300

    Changed SclkSpeed to ClkPrescaler and fixed format issues

--- STM32F4xx_Drivers/Drivers/Inc/stm32f407xx_spi_driver.h
+++ STM32F4xx_Drivers/Drivers/Inc/stm32f407xx_spi_driver.h
@@ -15,81 +15,81 @@
 /* -------------------------- GENERIC MACROS -------------------------- */
 
 /**
- * @defgroup SPI_DEVICE_MODES  Device Modes
+ * @defgroup SPI_DEVICE_MODES     Device Modes
  * @{
  */
-#define SPI_DEVICE_MODE_SLAVE  0
-#define SPI_DEVICE_MODE_MASTER 1
+#define SPI_DEVICE_MODE_SLAVE     0
+#define SPI_DEVICE_MODE_MASTER    1
 /** @} */
 
 /**
- * @defgroup SPI_BUS_CONFIG    Bus Configuration
+ * @defgroup SPI_BUS_CONFIG       Bus Configuration
  * @{
  */
-#define SPI_BUS_CONFIG_FD      1      // full duplex
-#define SPI_BUS_CONFIG_HD      2      // half duplex
-#define SPI_BUS_CONFIG_RXONLY  3      // simplex Rx only
+#define SPI_BUS_CONFIG_FD         1    // full duplex
+#define SPI_BUS_CONFIG_HD         2    // half duplex
+#define SPI_BUS_CONFIG_RXONLY     3    // simplex Rx only
 /** @} */
 
 /**
- * @defgroup SPI_SCLK_SPEED    Serial Clock Speed
+ * @defgroup SPI_SCLK_SPEED       Serial Clock Speed
  * @{
  */
-#define SPI_SCLK_SPEED_DIV2    0
-#define SPI_SCLK_SPEED_DIV4    1
-#define SPI_SCLK_SPEED_DIV8    2
-#define SPI_SCLK_SPEED_DIV16   3
-#define SPI_SCLK_SPEED_DIV32   4
-#define SPI_SCLK_SPEED_DIV64   5
-#define SPI_SCLK_SPEED_DIV128  6
-#define SPI_SCLK_SPEED_DIV256  7
+#define SPI_CLK_PRESCALER_DIV2    0
+#define SPI_CLK_PRESCALER_DIV4    1
+#define SPI_CLK_PRESCALER_DIV8    2
+#define SPI_CLK_PRESCALER_DIV16   3
+#define SPI_CLK_PRESCALER_DIV32   4
+#define SPI_CLK_PRESCALER_DIV64   5
+#define SPI_CLK_PRESCALER_DIV128  6
+#define SPI_CLK_PRESCALER_DIV256  7
 /** @} */
 
 /**
- * @defgroup SPI_DFF           Data Frame Format
+ * @defgroup SPI_DFF              Data Frame Format
  * @{
  */
-#define SPI_DFF_8_BITS         0
-#define SPI_DFF_16_BITS        1
+#define SPI_DFF_8_BITS            0
+#define SPI_DFF_16_BITS           1
 /** @} */
 
 /**
- * @defgroup SPI_CPOL          Clock Polarity
+ * @defgroup SPI_CPOL             Clock Polarity
  * @{
  */
-#define SPI_CPOL_LOW           0
-#define SPI_CPOL_HIGH          1
+#define SPI_CPOL_LOW              0
+#define SPI_CPOL_HIGH             1
 /** @} */
 
 /**
- * @defgroup SPI_CPHA          Clock Phase
+ * @defgroup SPI_CPHA             Clock Phase
  * @{
  */
-#define SPI_CPHA_FIRST_EDGE    0
-#define SPI_CPHA_SECOND_EDGE   1
+#define SPI_CPHA_FIRST_EDGE       0
+#define SPI_CPHA_SECOND_EDGE      1
 /** @} */
 
 /**
- * @defgroup SPI_SSM           Software Slave Management
+ * @defgroup SPI_SSM              Software Slave Management
  * @{
  */
-#define SPI_SSM_DI             0
-#define SPI_SSM_EN             1
+#define SPI_SSM_DI                0
+#define SPI_SSM_EN                1
 /** @} */
 
 /**
- * @defgroup SPI_FLAGS         Status Flags
+ * @defgroup SPI_FLAGS            Status Flags
  * @{
  */
-#define SPI_TXE_FLAG           (1 << SPI_SR_TXE_POS)        // Tx Buffer Empty Flag
-#define SPI_RXNE_FLAG          (1 << SPI_SR_RXNE_POS)       // Rx Buffer Not Empty Flag
-#define SPI_BSY_FLAG           (1 << SPI_SR_BSY_POS)        // Busy In Communication Flag
-#define SPI_FRE_FLAG           (1 << SPI_SR_FRE_POS)        // Frame Format Error Flag
-#define SPI_OVR_FLAG           (1 << SPI_SR_OVR_POS)        // Overrun Flag
-#define SPI_MODF_FLAG          (1 << SPI_SR_MODF_POS)       // Mode Fault Flag
-#define SPI_CRCERR_FLAG        (1 << SPI_SR_CRCERR_POS)     // CRC Error Flag
-#define SPI_UDR_FLAG           (1 << SPI_SR_UDR_POS)        // Underrun Flag
-#define SPI_CHSIDE_FLAG        (1 << SPI_SR_CHSIDE_POS)     // Channel Side Flag
+#define SPI_TXE_FLAG              (1 << SPI_SR_TXE_POS)        // Tx Buffer Empty Flag
+#define SPI_RXNE_FLAG             (1 << SPI_SR_RXNE_POS)       // Rx Buffer Not Empty Flag
+#define SPI_BSY_FLAG              (1 << SPI_SR_BSY_POS)        // Busy In Communication Flag
+#define SPI_FRE_FLAG              (1 << SPI_SR_FRE_POS)        // Frame Format Error Flag
+#define SPI_OVR_FLAG              (1 << SPI_SR_OVR_POS)        // Overrun Flag
+#define SPI_MODF_FLAG             (1 << SPI_SR_MODF_POS)       // Mode Fault Flag
+#define SPI_CRCERR_FLAG           (1 << SPI_SR_CRCERR_POS)     // CRC Error Flag
+#define SPI_UDR_FLAG              (1 << SPI_SR_UDR_POS)        // Underrun Flag
+#define SPI_CHSIDE_FLAG           (1 << SPI_SR_CHSIDE_POS)     // Channel Side Flag
 /** @} */
 
 /* -------------------------------------------------------------------- */
@@ -99,13 +99,13 @@
 
 /** @brief Configuration structure for SPI */
 typedef struct {
-	uint8_t SPI_DeviceMode;    /**< possible values from @ref SPI_DEVICE_MODES */
-	uint8_t SPI_BusConfig;     /**< possible values from @ref SPI_BUS_CONFIG   */
-	uint8_t SPI_SCLKSpeed;     /**< possible values from @ref SPI_SCLK_SPEED   */
-	uint8_t SPI_DFF;           /**< possible values from @ref SPI_DFF          */
-	uint8_t SPI_CPOL;          /**< possible values from @ref SPI_CPOL         */
-	uint8_t SPI_CPHA;          /**< possible values from @ref SPI_CPHA         */
-	uint8_t SPI_SSM;           /**< possible values from @ref SPI_SSM          */
+	uint8_t SPI_DeviceMode;       /**< possible values from @ref SPI_DEVICE_MODES */
+	uint8_t SPI_BusConfig;        /**< possible values from @ref SPI_BUS_CONFIG   */
+	uint8_t SPI_ClkPrescaler;     /**< possible values from @ref SPI_SCLK_SPEED   */
+	uint8_t SPI_DFF;              /**< possible values from @ref SPI_DFF          */
+	uint8_t SPI_CPOL;             /**< possible values from @ref SPI_CPOL         */
+	uint8_t SPI_CPHA;             /**< possible values from @ref SPI_CPHA         */
+	uint8_t SPI_SSM;              /**< possible values from @ref SPI_SSM          */
 } SPI_Config_t;
 
 /** @brief Handle structure for SPI */
@@ -121,7 +121,7 @@ typedef struct {
 
 uint8_t SPI_GetFlagStatus(SPI_RegDef_t *pSPIx, uint32_t FlagName);
 
-void SPI_PCLKControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi);
+void SPI_PclkControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi);
 
 void SPI_Init(SPI_Handle_t *pSPIHandle);
 void SPI_DeInit(SPI_RegDef_t *pSPIx);
