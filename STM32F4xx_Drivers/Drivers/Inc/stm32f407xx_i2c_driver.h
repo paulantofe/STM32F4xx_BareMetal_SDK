8282212647d250cc686b869896bf7c7082277078
Paul Antofe <paul.antofe@gmail.com>
2026-09-02 13:18:37 +0300

    Added API prototypes

--- STM32F4xx_Drivers/Drivers/Inc/stm32f407xx_i2c_driver.h
+++ STM32F4xx_Drivers/Drivers/Inc/stm32f407xx_i2c_driver.h
@@ -56,6 +56,24 @@ typedef struct {
 
 
 /* -------------------------- SUPPORTED APIs (for more information check the function definitions) -------------------------- */
+
+uint8_t I2C_GetFlagStatus(I2C_RegDef_t *pI2Cx, uint32_t FlagName);
+
+void I2C_PeripheralControl(I2C_RegDef_t *pI2Cx, uint8_t EnorDi);
+
+void I2C_PClkControl(I2C_RegDef_t *pI2Cx, uint8_t EnorDi);
+
+void I2C_Init(I2C_Handle_t *pI2CHandle);
+void I2C_DeInit(I2C_RegDef_t *pI2Cx);
+
+void I2C_MasterSendData(I2C_Handle_t *pI2CHandle, uint8_t *pTxBuffer, uint32_t Len, uint8_t SlaveAddr, uint8_t Sr);
+void I2C_MasterReceiveData(I2C_Handle_t *pI2CHandle, uint8_t *pRxBuffer, uint32_t Len, uint8_t SlaveAddr, uint8_t Sr);
+void I2C_SlaveSendData(I2C_RegDef_t *pI2Cx, uint8_t data);
+uint8_t I2C_SlaveReceiveData(I2C_RegDef_t *pI2Cx);
+
+void I2C_IRQInterruptConfig(IRQn_Type IRQNumber, uint8_t EnorDi);
+void I2C_IRQPriorityConfig(IRQn_Type IRQNumber, uint8_t IRQPriority);
+
 /* -------------------------------------------------------------------------------------------------------------------------- */
 
 #endif
