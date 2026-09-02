f3e00e3ad278f22a4d793a77d421d6edac1b0657
Paul Antofe <paul.antofe@gmail.com>
2026-09-02 13:05:55 +0300

    Added generic macros for I2C

--- STM32F4xx_Drivers/Drivers/Inc/stm32f407xx_i2c_driver.h
+++ STM32F4xx_Drivers/Drivers/Inc/stm32f407xx_i2c_driver.h
@@ -11,6 +11,28 @@
 #include "stm32f407xx.h"
 
 /* -------------------------- GENERIC MACROS -------------------------- */
+
+/** @defgroup I2C_SCL_SPEED    Speed of I2C Serial Clock
+ * @{
+ */
+#define I2C_SCL_SPEED_SM       100000U   // Standard Mode 100 kHz
+#define I2C_SCL_SPEED_FM       400000U   // Fast Mode     400 kHz
+/** @} */
+
+/** @defgroup I2C_ACK_CTRL     Enable/Disable ACK
+ * @{
+ */
+#define I2C_ACK_EN             1
+#define I2C_ACK_DI             0
+/** @} */
+
+/** @defgroup I2C_FM_DUTY      Duty Cycle for Fast Mode
+ * @{
+ */
+#define I2C_FM_DUTY_2          0
+#define I2C_FM_DUTY_16_9       1
+/** @} */
+
 /* -------------------------------------------------------------------- */
 
 
@@ -18,10 +40,10 @@
 
 /** @brief Configuration structure for I2C */
 typedef struct {
-	uint32_t I2C_SclSpeed;
-	uint8_t  I2C_DeviceAddress;
-	uint8_t  I2C_AckControl;
-	uint16_t I2C_FMDutyCycle;
+	uint32_t I2C_SclSpeed;          /**< possible values from @ref I2C_SCL_SPEED    */
+	uint8_t  I2C_DeviceAddress;     /**< configurable by the user                   */
+	uint8_t  I2C_AckControl;        /**< possible values from @ref I2C_ACK_CTRL     */
+	uint16_t I2C_FMDutyCycle;       /**< possible values from @ref I2C_FM_DUTY      */
 } I2C_Config_t;
 
 /** @brief Handle structure for I2C */
