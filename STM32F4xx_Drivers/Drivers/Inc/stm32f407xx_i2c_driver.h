b1f3213c3444586c5924d7ac9164e4a7926e4b8c
Paul Antofe <paul.antofe@gmail.com>
2026-09-08 20:49:13 +0300

    Changed interrupt communication APIs signature to return state

--- STM32F4xx_Drivers/Drivers/Inc/stm32f407xx_i2c_driver.h
+++ STM32F4xx_Drivers/Drivers/Inc/stm32f407xx_i2c_driver.h
@@ -116,8 +116,8 @@ void I2C_DeInit(I2C_RegDef_t *pI2Cx);
 
 void I2C_MasterSendData(I2C_Handle_t *pI2CHandle, uint8_t *pTxBuffer, uint32_t Len, uint8_t SlaveAddr, uint8_t Sr);
 void I2C_MasterReceiveData(I2C_Handle_t *pI2CHandle, uint8_t *pRxBuffer, uint32_t Len, uint8_t SlaveAddr, uint8_t Sr);
-void I2C_MasterSendDataIT(I2C_Handle_t *pI2CHandle, uint8_t *pTxBuffer, uint32_t Len, uint8_t SlaveAddr, uint8_t Sr);
-void I2C_MasterReceiveDataIT(I2C_Handle_t *pI2CHandle, uint8_t *pRxBuffer, uint32_t Len, uint8_t SlaveAddr, uint8_t Sr);
+uint8_t I2C_MasterSendDataIT(I2C_Handle_t *pI2CHandle, uint8_t *pTxBuffer, uint32_t Len, uint8_t SlaveAddr, uint8_t Sr);
+uint8_t I2C_MasterReceiveDataIT(I2C_Handle_t *pI2CHandle, uint8_t *pRxBuffer, uint32_t Len, uint8_t SlaveAddr, uint8_t Sr);
 
 void I2C_IRQInterruptConfig(IRQn_Type IRQNumber, uint8_t EnorDi);
 void I2C_IRQPriorityConfig(IRQn_Type IRQNumber, uint8_t IRQPriority);
--- STM32F4xx_Drivers/Drivers/Src/stm32f407xx_i2c_driver.c
+++ STM32F4xx_Drivers/Drivers/Src/stm32f407xx_i2c_driver.c
@@ -351,14 +351,14 @@ void I2C_MasterReceiveData(I2C_Handle_t *pI2CHandle, uint8_t *pRxBuffer, uint32_
  * @param
  * @retval
  */
-void I2C_MasterSendDataIT(I2C_Handle_t *pI2CHandle, uint8_t *pTxBuffer, uint32_t Len, uint8_t SlaveAddr, uint8_t Sr);
+uint8_t I2C_MasterSendDataIT(I2C_Handle_t *pI2CHandle, uint8_t *pTxBuffer, uint32_t Len, uint8_t SlaveAddr, uint8_t Sr);
 
 /**
  * @brief
  * @param
  * @retval
  */
-void I2C_MasterReceiveDataIT(I2C_Handle_t *pI2CHandle, uint8_t *pRxBuffer, uint32_t Len, uint8_t SlaveAddr, uint8_t Sr);
+uint8_t I2C_MasterReceiveDataIT(I2C_Handle_t *pI2CHandle, uint8_t *pRxBuffer, uint32_t Len, uint8_t SlaveAddr, uint8_t Sr);
 
 /**
  * @brief
