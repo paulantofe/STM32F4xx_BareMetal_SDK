b3292f6299862a2c90566276e23072166f758e97
Paul Antofe <paul.antofe@gmail.com>
2026-09-08 20:45:00 +0300

    Added interrupt driven APIs prototypes

--- STM32F4xx_Drivers/Drivers/Src/stm32f407xx_i2c_driver.c
+++ STM32F4xx_Drivers/Drivers/Src/stm32f407xx_i2c_driver.c
@@ -351,14 +351,14 @@ void I2C_MasterReceiveData(I2C_Handle_t *pI2CHandle, uint8_t *pRxBuffer, uint32_
  * @param
  * @retval
  */
-void I2C_SlaveSendData(I2C_RegDef_t *pI2Cx, uint8_t data);
+void I2C_MasterSendDataIT(I2C_Handle_t *pI2CHandle, uint8_t *pTxBuffer, uint32_t Len, uint8_t SlaveAddr, uint8_t Sr);
 
 /**
  * @brief
  * @param
  * @retval
  */
-uint8_t I2C_SlaveReceiveData(I2C_RegDef_t *pI2Cx);
+void I2C_MasterReceiveDataIT(I2C_Handle_t *pI2CHandle, uint8_t *pRxBuffer, uint32_t Len, uint8_t SlaveAddr, uint8_t Sr);
 
 /**
  * @brief
