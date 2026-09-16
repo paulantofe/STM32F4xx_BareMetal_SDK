7ad48dd02c99cd5a2a26827af87e6acae6f3501e
Paul Antofe <paul.antofe@gmail.com>
2026-09-16 12:30:05 +0300

    Added Configuration Strucutres for USART

--- STM32F4xx_Drivers/Drivers/Inc/stm32f407xx_usart_driver.h
+++ STM32F4xx_Drivers/Drivers/Inc/stm32f407xx_usart_driver.h
@@ -15,6 +15,23 @@
 
 
 /* -------------------------- CONFIGURATION STRUCTURES -------------------------- */
+
+/** @brief Configuration structure for USART */
+typedef struct {
+	uint32_t USART_Baudrate;
+	uint8_t  USART_Mode;
+	uint8_t  USART_NoOfStopBits;
+	uint8_t  USART_WordLen;
+	uint8_t  USART_ParityControl;
+	uint8_t  USART_HWFlowControl;
+} USART_Config_t;
+
+/** @brief Handle structure for USART */
+typedef struct {
+	USART_Config_t USART_Config;
+	USART_RegDef_t *pUSARTx;
+} USART_Handle_t;
+
 /* ------------------------------------------------------------------------------ */
 
 
