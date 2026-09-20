b9b5257806de2e0f7c7f93a787924e01c4779b30
Paul Antofe <paul.antofe@gmail.com>
2026-09-20 18:27:55 +0300

    Added RCC header file

--- STM32F4xx_Drivers/Drivers/Inc/stm32f407xx_rcc_driver.h
+++ STM32F4xx_Drivers/Drivers/Inc/stm32f407xx_rcc_driver.h
@@ -1,13 +1,20 @@
-/*
- * stm32f407xx_rcc_driver.h
+/**
+ * @file 		stm32f407xx_rcc_driver.h
+ * @brief 		Header file for RCC peripheral support
  *
- *  Created on: Sep 20, 2026
- *      Author: paula
+ * @details     This header file provides APIs to get the clock source of APB1 and APB2 busses of the MCU.
  */
 
 #ifndef INC_STM32F407XX_RCC_DRIVER_H_
 #define INC_STM32F407XX_RCC_DRIVER_H_
 
+#include "stm32f407xx.h"
 
+/* -------------------------- SUPPORTED APIs (for more information check the function definitions) -------------------------- */
 
-#endif /* INC_STM32F407XX_RCC_DRIVER_H_ */
+uint32_t RCC_GetPCLK1Value(void);
+uint32_t RCC_GetPCLK2Value(void);
+
+/* -------------------------------------------------------------------------------------------------------------------------- */
+
+#endif
