d9e72a32ccbd68103b07c4320c9b57c0f61368dc
paulantofe <paul.antofe@gmail.com>
2026-09-21 14:50:54 +0300

    Added I2C and USART driver includes to main.c

--- STM32F4xx_Drivers/Src/main.c
+++ STM32F4xx_Drivers/Src/main.c
@@ -1,11 +1,15 @@
 #include "stm32f407xx.h"
 #include "stm32f407xx_gpio_driver.h"
 #include "stm32f407xx_spi_driver.h"
+#include "stm32f407xx_i2c_driver.h"
+#include "stm32f407xx_usart_driver.h"
 
 int main(void) {
 
-
+	
 	while (1) {
 
 	}
+
+	return 0;
 }
