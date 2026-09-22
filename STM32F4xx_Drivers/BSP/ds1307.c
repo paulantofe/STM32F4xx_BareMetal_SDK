31cbe561170dc114c9e961bc03ec4e9102c39cb9
Paul Antofe <paul.antofe@gmail.com>
2026-09-22 18:33:50 +0300

    Added basic structure to ds1307 source file

---
+++ STM32F4xx_Drivers/BSP/ds1307.c
@@ -0,0 +1,23 @@
+/**
+ * @file 		ds1307.c
+ * @brief 		Source file for the DS1307 Real-Time Clock BSP driver.
+ *
+ * @details     This source file contains the implementation of the APIs for communicating with the DS1307
+ *              RTC module over I2C. It includes functional code for device initialization, reading and
+ *              writing time and date registers, as well as helper functions for BCD (Binary-Coded Decimal)
+ *              conversions.
+ */
+
+#include "ds1307.h"
+
+/* -------------------------- PRIVATE HELPER FUNCTIONS -------------------------- */
+
+
+/* ----------------------------------------------------------------------------------- */
+
+
+/* -------------------------- SUPPORTED APIs IMPLEMENTATION -------------------------- */
+
+
+
+/* ----------------------------------------------------------------------------------- */
