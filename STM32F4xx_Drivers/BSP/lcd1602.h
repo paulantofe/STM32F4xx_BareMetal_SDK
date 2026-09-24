f956152c051a6362a74f82697b6951bcaeb8564e
Paul Antofe <paul.antofe@gmail.com>
2026-09-24 10:45:45 +0300

    Added some macros and init prototype

--- STM32F4xx_Drivers/BSP/lcd1602.h
+++ STM32F4xx_Drivers/BSP/lcd1602.h
@@ -13,14 +13,26 @@
 
 /* -------------------------- GENERIC MACROS -------------------------- */
 
-
+/**
+ * @defgroup LCD1602_APP_CFG   Application Configurable Items
+ * @{
+ */
+#define LCD1602_GPIO_PORT      GPIOD
+#define LCD1602_GPIO_RS        GPIO_PIN_NO_0
+#define LCD1602_GPIO_RW        GPIO_PIN_NO_1
+#define LCD1602_GPIO_EN        GPIO_PIN_NO_2
+#define LCD1602_GPIO_D4        GPIO_PIN_NO_3
+#define LCD1602_GPIO_D5        GPIO_PIN_NO_4
+#define LCD1602_GPIO_D6        GPIO_PIN_NO_5
+#define LCD1602_GPIO_D7        GPIO_PIN_NO_6
+/** @} */
 
 /* -------------------------------------------------------------------- */
 
 
 /* -------------------------- SUPPORTED APIs (for more information check the function definitions) -------------------------- */
 
-
+void LCD1602_Init(void);
 
 /* -------------------------------------------------------------------------------------------------------------------------- */
 
