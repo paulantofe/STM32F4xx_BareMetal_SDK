8b6199ec1d691ce90d51268fc99530550f2c2144
Paul Antofe <paul.antofe@gmail.com>
2026-09-24 10:23:35 +0300

    Added basic structure to lcd1602 files

---
+++ STM32F4xx_Drivers/BSP/lcd1602.h
@@ -0,0 +1,27 @@
+/**
+ * @file 		lcd1602.h
+ * @brief 		Board Support Package (BSP) driver header for the LCD1602 display.
+ *
+ * @details
+ */
+
+#ifndef INC_LCD1602_H_
+#define INC_LCD1602_H_
+
+#include "stm32f407xx.h"
+#include "stm32f407xx_gpio_driver.h"
+
+/* -------------------------- GENERIC MACROS -------------------------- */
+
+
+
+/* -------------------------------------------------------------------- */
+
+
+/* -------------------------- SUPPORTED APIs (for more information check the function definitions) -------------------------- */
+
+
+
+/* -------------------------------------------------------------------------------------------------------------------------- */
+
+#endif  INC_LCD1602_H_
