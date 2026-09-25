400f54ce1ac2a3827ec4cfd6f5538122909a85c5
Paul Antofe <paul.antofe@gmail.com>
2026-09-25 12:17:21 +0300

    Added usart demo app file

---
+++ STM32F4xx_Drivers/Src/DemoApps/USARTDemoApp.c
@@ -0,0 +1,29 @@
+/**
+ * @file 		USARTDemoApp.c
+ * @brief 	    STM32F407xx Bare-Metal USART driver demo
+ *
+ * @details
+ *
+ * @note        TO RUN THIS DEMO: Ensure that the default main.c is excluded from build and this file is included in the project`s
+ *              source path.
+ *
+ * Hardware Setup:
+ * - Arduino Board
+ * - LCD1602 Display
+ */
+
+#include "stm32f407xx.h"
+
+#define true      1
+#define false     0
+
+int main(void) {
+
+
+    while (true) {
+
+    }
+
+	return 0;
+}
+
