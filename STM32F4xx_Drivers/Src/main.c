be32de1e5175faefe032e9841e5d3009e7958afe
Paul Antofe <paul.antofe@gmail.com>
2026-09-25 17:25:02 +0300

    Updated template main.c

--- STM32F4xx_Drivers/Src/main.c
+++ STM32F4xx_Drivers/Src/main.c
@@ -1,13 +1,26 @@
+/**
+ * @file        main.c
+ * @brief       Main application template for STM32F407xx Bare-Metal SDK
+ *
+ * @details     This is a blank template containing includes for all standard
+ *              on-board peripherals and external device drivers. Use this
+ *              as the starting point for new applications.
+ */
+
 #include "stm32f407xx.h"
 #include "stm32f407xx_gpio_driver.h"
 #include "stm32f407xx_spi_driver.h"
 #include "stm32f407xx_i2c_driver.h"
 #include "stm32f407xx_usart_driver.h"
 
+// External modules
+#include "lcd1602.h"
+#include "ds1307.h"
+
 int main(void) {
 
-	
-	while (1) {
+
+	while (true) {
 
 	}
 
